69a8349b8252f9f78b7b01d31229fe0fe113aa66
Great Adams <kingifean@gmail.com>
2022-11-29 12:40:50 +0000

    Added datatypes and vars

--- var.js
+++ var.js
@@ -1,1 +1,18 @@
-// Welcome
+// Data types
+
+// Strings
+// Numbers
+// Boolens
+// Arrays
+// Objects
+
+// Comments 
+
+
+// Var
+
+let firstName = 'Adams'
+
+const lastName = 'Great'
+
+var age = 34
