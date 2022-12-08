681aec5c9530e42afa324dede78cdef027365718
Great Adams <kingifean@gmail.com>
2022-12-08 14:05:36 +0100

    strings

---
+++ scope.js
@@ -0,0 +1,24 @@
+// Name comventions
+// carmel casing
+
+// myName
+// getBooksArt
+// greatAdamsIfeanyiChukwu
+
+// assignment diff
+
+// const, let , var
+
+// var declare,  //name of var,   // assignment pro     // value
+const             car            =              'Adams'
+
+const lastName = 'Adams'
+let firstName = 'Great'
+var middleName = 'London'
+
+
+firstName = 'Ifeanyichukwu'
+middleName = 'John'
+// lastName = 'Dave'
+
+
