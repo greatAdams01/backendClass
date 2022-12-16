15c7206625bcdaaac483999db395f5516f8254ba
Great Adams <kingifean@gmail.com>
2022-12-16 11:59:06 +0100

    number boolens

---
+++ boolens.js
@@ -0,0 +1,22 @@
+// ture & false
+// 1 & 0
+// null, undefined -> false
+
+// Number, String, Array, Boolean, Object
+
+
+const age = 60
+if (age < 18) {
+  console.log('Your not allowed to play this game!!!')
+} else if(age > 60){
+  console.log('Your over this game!!!')
+} else {
+  console.log(`Well!! grab a sit let's play`)
+}
+
+
+if (undefined) {
+  console.log('No')
+} else {
+  console.log('Yes')
+}
--- strings.js
+++ strings.js
@@ -3,11 +3,35 @@
 let msg = 'Am running out of battrie'
 let anotherMsg = new String('anytin')
 
+const names = ['Adams', 'Great', 'John', 'Daivid']
+
+for (let index = 0; index <= names.length; index++) {
+
+  console.log(names[index])
+}
+
 
 
 // console.log(msg.length, anotherMsg.length)
 
 // console.log(msg.includes('cook'))
 
-console.log(msg.toUpperCase())
+// console.log(msg.toLowerCase())
 
+const lastName = 'Adams'
+const firstName = 'Great'
+// const fullName = firstName.concat(lastName)
+
+let fullName = firstName + lastName + lastName.length
+console.log(fullName)
+let myText = "I'm just going to quote dad \"Your my start\" "
+// myText = 'I'm just going to quote dad "" '
+myText = `Total count of my full name is ${ fullName.length } characters  `
+
+console.log(myText)
+
+
+
+fullName = ``
+
+// console.log(fullName)
